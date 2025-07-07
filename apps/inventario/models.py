e1c5842b0e5a3bb806b0a441a82e7ce8aab9c47b
achuzuzu <azulroa@ctpantoniomarte.edu.ar>
2025-07-07 17:20:58 -0300

    agregue atributo,tipo

--- apps/inventario/models.py
+++ apps/inventario/models.py
@@ -3,6 +3,8 @@ from django.db import models
 class Categoria(models.Model):
 	nombre = models.CharField(max_length=100)
 	descripcion = models.CharField(max_length = 100)
+	tipo = models.CharField(max_length= 100)
+	
 	
 	def __str__(self):
 		return f"{self.nombre}"
